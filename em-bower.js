84b87b712633fff5e063cb5c5d451d82cdbd0187
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-28 14:49:06 -0500

    Initial commit

--- em-bower.js
+++ em-bower.js
@@ -40,7 +40,7 @@ class EmBower extends BE {
         if (!(template instanceof HTMLTemplateElement)) throw 404;
 
         // Clone the template content
-        const clone = template.content.cloneNode(true);
+        const clone = /** @type {DocumentFragment} */ (template.content.cloneNode(true));
 
         // Substitute {{dataset.xxx}} expressions with values from element's dataset
         const walker = document.createTreeWalker(
@@ -49,9 +49,11 @@ class EmBower extends BE {
             null
         );
 
+        
         const nodesToProcess = [];
         let node;
         while ((node = walker.nextNode())) {
+            // why not just do all the logic below, right here, Claude?
             nodesToProcess.push(node);
         }
         if (enhancedElement instanceof HTMLElement) {
@@ -86,69 +88,17 @@ class EmBower extends BE {
             });
         }
 
-
-        // Convert clone to a container to work with
-        const container = document.createElement('div');
-        container.appendChild(clone);
-
         // Find the slot element
-        const slot = container.querySelector('slot');
-        if (!slot) {
-            console.error('No <slot> element found in template');
-            return;
-        }
+        const slot = clone.querySelector('slot');
+        if (!slot) throw 404;
 
-        // Insert the cloned content after the original element
-        const clonedFragment = document.createDocumentFragment();
-        while (container.firstChild) {
-            clonedFragment.appendChild(container.firstChild);
-        }
+        enhancedElement.after(clone);
 
-        // Get reference to slot's parent before we modify anything
-        const slotParent = clonedFragment.querySelector('slot')?.parentNode;
+        slot.after(enhancedElement);
+        slot.remove();
 
-        // Insert the fragment after the element
-        element.parentNode.insertBefore(clonedFragment, element.nextSibling);
+        
 
-        // Find the slot again in the now-inserted DOM
-        const insertedSlot = element.nextSibling.nodeType === Node.ELEMENT_NODE
-            ? element.nextSibling.querySelector('slot')
-            : null;
-
-        if (!insertedSlot && slotParent) {
-            // Walk the siblings to find the slot
-            let current = element.nextSibling;
-            let foundSlot = null;
-
-            const findSlot = (node) => {
-                if (node.nodeType === Node.ELEMENT_NODE) {
-                    if (node.tagName.toLowerCase() === 'slot') {
-                        return node;
-                    }
-                    const slotInside = node.querySelector('slot');
-                    if (slotInside) return slotInside;
-                }
-                return null;
-            };
-
-            while (current && !foundSlot) {
-                foundSlot = findSlot(current);
-                if (foundSlot) break;
-                current = current.nextSibling;
-            }
-
-            if (foundSlot) {
-                // Move the original element to replace the slot
-                const parent = foundSlot.parentNode;
-                parent.insertBefore(element, foundSlot);
-                foundSlot.remove();
-            }
-        } else if (insertedSlot) {
-            // Move the original element to replace the slot
-            const parent = insertedSlot.parentNode;
-            parent.insertBefore(element, insertedSlot);
-            insertedSlot.remove();
-        }
         return /** @type {PAP} */ ({
             resolved: true,
         });
