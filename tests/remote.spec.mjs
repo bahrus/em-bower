21280743fdb00edd93a55e6d51c241b21d17db58
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-11-29 15:41:32 -0500

    0.0.2

---
+++ tests/remote.spec.mjs
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+test('local', async ({ page }) => {
+    await page.goto('./tests/remote.html');
+    // wait for 1 second
+    await page.waitForTimeout(2000);
+    const editor = page.locator('#target');
+    await expect(editor).toHaveAttribute('mark', 'good');
+});
